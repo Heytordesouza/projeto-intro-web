4533a1d781efdf53cf28c272a2ae815793e977c3
Heytordesouza <heytordesnascimento@gmail.com>
2022-09-09 14:41:21 -0300

    Semana 2 resolvido

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ Recomendamos, pensando no futuro, que você também pense em dados que podem
 receber múltiplos valores de uma só vez. Não vamos usar estes dados até mais 
 pra frente, mas é legal já ter uma ideia.*/
 
-//const carro = "Modelo do carro"
+//const modelo = "Modelo do carro"
 //const valor = Number "Valor do carro"
 //const km = boolean "Se o carro é 0Km"
 
@@ -40,40 +40,41 @@ const verificouEmail2 = true;
 
 const nome3 = "Beltrano";
 const idade3 = 27;
-const verificouEmail3 = true;*/
+const verificouEmail3 = true;
+_______________________________________________________________________________
 
-const carro1 = "Chevrolet Onix 1.0 MT"
+const modelo1 = "Chevrolet Onix 1.0 MT"
 const valor1 = 78.699
 const km1 = true
 const cores1 = ["Preto", "Branco", "Cinza", "Prata", "Azul"]
 
-const carro2 = "Hyundai HB20 Sense 1.0"
+const modelo2 = "Hyundai HB20 Sense 1.0"
 const valor2 = 76.699
 const km2 = true
 const cores2 = ["Preto", "Prata", "Branco", "Cinza"]
 
-const carro3 = "Renault Logan Authentique Flex 1.0 2018"
+const modelo3 = "Renault Logan Authentique Flex 1.0 2018"
 const valor3 = 46.367
 const km3 = false
 const cores3 = ["Preto", "Prata", "Branco", "Vermelho"]
 
-/*3. Faça um cálculo de média, entre os valores numéricos respectivos de cada item. 
+3. Faça um cálculo de média, entre os valores numéricos respectivos de cada item. 
 Imprima o valor da média utilizando um console.log. Guarde este valor em uma const;
 💡A média deve ser igual à soma dos itens, dividida pelo total de itens.*/
 
-const media = ((valor1 + valor2 + valor3)/3)
-console.log(media) //67.255
+//const media = ((valor1 + valor2 + valor3)/3)
+//console.log(media) //67.255
 
 
 /*4. Com um console.log, imprima o resultado de uma operação lógica que checa se 
 todos os valores de variáveis booleanas criadas até aqui são verdadeiras.
 💡 Exemplo
 const verificaEmails = verificouEmail1 && verificouEmail2 && verificouEmail3;
-console.log(verificaEmails);
+console.log(verificaEmails);*/
 // nesse caso, vai imprimir false, pois verificouEmail1 === false*/
 
-const kms = Boolean(km1 & km2 & km3)
-console.log(kms) //false
+//const kms = Boolean(km1 & km2 & km3)
+//console.log(kms) //false
 
 
 /*5. Crie pelo menos mais uma característica para o item que você criou. 
@@ -84,7 +85,7 @@ const idade; //number
 const verificouEmail; //boolean
 const projetos; //array de strings*/
 
-
+//Suas cores
 
 /*6. Exiba um relatório utilizando console.log(), mostrando todos os dados de 
 cada um dos itens criados até aqui
@@ -93,20 +94,121 @@ cada um dos itens criados até aqui
 FULANO
 idade: 33
 verificou email?: false
-projetos: ["Projeto de HTML", "Projeto de CSS", "Projeto React"] */
+projetos: ["Projeto de HTML", "Projeto de CSS", "Projeto React"]
 
 console.log(`
-${carro1.toUpperCase()}
+${modelo1.toUpperCase()}
 Valor: R$${valor1}
 O Carro é 0Km? ${km1}
 Suas cores são: ${cores1}
 
-${carro2.toUpperCase()}
+${modelo2.toUpperCase()}
 Valor: R$${valor2}
 O Carro é 0Km? ${km2}
 Suas cores são: ${cores2}
 
-${carro1.toUpperCase()}
+${modelo3.toUpperCase()}
 Valor: R$${valor3}
 O Carro é 0Km? ${km3}
 Suas cores são: ${cores3}`)
+
+Semana 2
+
+1. Transforme os itens que criamos nas últimas semanas em objetos.
+
+Exemplo:
+
+//ANTES
+const nome1 = "Fulano";
+const idade1 = 33;
+const verificouEmail1 = false; 
+//DEPOIS
+objeto1 = { 
+  nome: "Fulano", 
+  idade: 33,
+  verificouEmail: false
+} */
+
+
+carro1 = {
+modelo1: "Chevrolet Onix 1.0 MT",
+valor1: 78.699,
+km1: true,
+cores1: ["Preto", "Branco", "Cinza", "Prata", "Azul"]
+}
+
+carro2 = {
+modelo2: "Hyundai HB20 Sense 1.0",
+valor2: 76.699,
+km2: true,
+cores2: ["Preto", "Prata", "Branco", "Cinza"]
+}
+
+carro3 = {
+modelo3: "Renault Logan Authentique Flex 1.0 2018",
+valor3: 46.367,
+km3: false,
+cores3: ["Preto", "Prata", "Branco", "Vermelho"]
+}
+
+
+//2. Crie um array para guardar os objetos. Este array deve estar vazio, por enquanto;
+
+const carros = []
+
+
+/*3. Adicione os objetos criados no item 1 ao array de objetos criado no item 2, 
+utilizando o push()
+
+Ao fim, você deve ter algo parecido com isso:
+[
+  { 
+    nome: "Fulano", 
+    idade: 33,
+    verificouEmail: false
+  },
+  { 
+    nome: "Beltrano", 
+    idade: 41,
+    verificouEmail: true
+  },
+  { 
+    nome: "Fulano", 
+    idade: 27,
+    verificouEmail: true
+  },
+]*/
+
+//carros.push(carro1, carro2, carro3)
+
+//console.log(carros)
+
+/*4. Altere o item “Adicione os novos objetos no array de objetos, 
+utilizando o push()” (item 3), para criar uma verificação antes de dar o push. 
+A caraterística booleana do objeto deve ser validada. 
+Isto é, o objeto só deve ser adicionado ao array se a propriedade booleana for true;
+
+5. Crie uma condição else, que, em caso de valor false na condição acima, 
+exiba um **ALERT** avisando para o usuário que o item não foi adicionado, e não faça o push*/
+
+if(carro1.km1) {
+  carros.push(carro1)
+  alert(`${carro1.modelo1} foi adicionado ao array`)
+}else {
+  alert(`${carro1.modelo1} não foi adicionado ao array`)
+}
+
+if(carro2.km2) {
+  carros.push(carro2)
+  alert(`${carro2.modelo2} foi adicionado ao array`)
+}else {
+  alert(`${carro2.modelo2} não foi adicionado ao array`)
+}
+
+if(carro3.km3) {
+  carros.push(carro3)
+  alert(`${carro3.modelo3} foi adicionado ao array`)
+}else {
+  alert(`${carro3.modelo3} não foi adicionado ao array`)
+}
+
